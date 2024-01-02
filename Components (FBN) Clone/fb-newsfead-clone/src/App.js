ec2ead2ca9aec7211cf526c4bab5ed047c73ab3c
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-02 11:20:44 -0700

    create component function

--- Components (FBN) Clone/fb-newsfead-clone/src/App.js
+++ Components (FBN) Clone/fb-newsfead-clone/src/App.js
@@ -1,25 +1,39 @@
 /////////// all import here /////////////
-import {useState} from 'react'
+import { useEffect, useState } from "react";
+
 function App() {
   /////////// all state here /////////////
+  const [product, setProduct] = useState([]);
 
   /////////// all function here /////////////
 
-  function fbClone() {
+  const getProduct = () => {
     fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then(res => res.json())
+      .then((res) => setProduct(res.products));
+  };
+console.log(product);
+  useEffect(() => {
+    getProduct();
+  },[]);
+
+  // console.log(product);
+
+  if(!product?.length) {
+return <div>Loading</div>
   }
 
-  fbClone() 
-
-
-  return <div className="App">
-    <div>
-      <h1>{data().title
-}</h1>
+  return (
+    <div className="App">
+      <div>
+        <h1>FBN CLONE</h1>
+       {product.map((item) =>  {
+        return <h1>{item.product?.brand}</h1>
+       })}
+        
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
