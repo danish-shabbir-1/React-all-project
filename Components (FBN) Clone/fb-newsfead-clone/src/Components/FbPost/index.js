54390df4795f585df176c32befb0d94e509d4627
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-03 11:51:07 -0700

    only left like share commit functionality

--- Components (FBN) Clone/fb-newsfead-clone/src/Components/FbPost/index.js
+++ Components (FBN) Clone/fb-newsfead-clone/src/Components/FbPost/index.js
@@ -3,18 +3,24 @@ import FbImageLibrary from "react-fb-image-grid";
 
 function fbPost(props) {
   return (
-    <div className="container">
+    <div className="container-one">
       <div className="fbContainer">
         <img className="prfImage" src={props.product.thumbnail} />
         <h3 className="Prf-name">{props.product.title}</h3>
       </div>
       <br />
-      <div>
+      <div className="Description">
+        {" "}
         <p>{props.product.description}</p>
+      </div>
+      <div>
         <FbImageLibrary
           className="fb-images-lib"
           images={props.product.images}
-        />
+        />{" "}
+      </div>
+      <div className="likes">
+      {/* <i class="fa-regular fa-thumbs-up" style="color: #ffffff;"></i> */}
       </div>
     </div>
   );
