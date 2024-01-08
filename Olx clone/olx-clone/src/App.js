70dd14c94a5bef53312c3043d308820e02973c69
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-08 05:41:31 -0700

    not clear error product map

--- Olx clone/olx-clone/src/App.js
+++ Olx clone/olx-clone/src/App.js
@@ -1,12 +1,28 @@
 import Navbar from './Components/Navbar';
-import products from './Components/cards';
+import Card from './Components/cards';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [Products, setProducts] = useState([])
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
+  const getAllProducts = () => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then(res => setProducts(res));
+  };
+console.log(Products);
   return (
     <div className="App">
   <Navbar />
-
+  {/* {Products.map(item => {
+    
+  })} */}
     </div>
   );
 }
--- Olx clone/olx-clone/src/Components/cards/index.js
+++ Olx clone/olx-clone/src/Components/cards/index.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { useEffect } from "react";
 
-useEffect(() => {
-  getAllProducts();
-}, []);
-
-const getAllProducts = () => {
-  fetch("https://dummyjson.com/products")
-    .then((res) => res.json())
-    .then(console.log);
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
-const products = () => {
-  return <div></div>;
-};
 
-export default products;
+export default Card;
