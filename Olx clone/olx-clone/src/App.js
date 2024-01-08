44dbd6122268fb5891e1586d05533fced0e18acb
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-08 04:55:30 -0700

    complete navbar

---
+++ Olx clone/olx-clone/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './Components/Navbar';
+import './App.css';
+// import { Navbar } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+  <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ Olx clone/olx-clone/src/Components/Navbar/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div>
+      <nav className="navbar bg-dark">
+        <div className="container-fluid">
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default navbar;
