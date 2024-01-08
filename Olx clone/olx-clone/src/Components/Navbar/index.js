1c35a1c916f5d6185d48ff7e8745dbcac298e1dc
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-08 11:59:28 -0700

    some edititing

--- Olx clone/olx-clone/src/Components/Navbar/index.js
+++ Olx clone/olx-clone/src/Components/Navbar/index.js
@@ -1,11 +1,13 @@
 import React from "react";
+import OlxLogo from './../../Olx Logo2.png'
+import './../cards/style.css'
 
 const navbar = () => {
   return (
     <div>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-dark container navbar-expand-lg bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <img src={OlxLogo} className="logo"/>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -13,12 +15,15 @@ const navbar = () => {
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+          Select Location
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Pakistan</a></li>
+            <li><hr class="dropdown-divider" /></li>
+            <li><a class="dropdown-item" href="#">Sindh</a></li>
+            <li><a class="dropdown-item" href="#">Punjab</a></li>
+            <li><a class="dropdown-item" href="#">KPK</a></li>
+            <li><a class="dropdown-item" href="#">Balochistan</a></li>
           </ul>
         </li>
       </ul>
--- Olx clone/olx-clone/src/Components/cards/index.js
+++ Olx clone/olx-clone/src/Components/cards/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './style.css'
+import { FontAwesomeIcon, } from "@fortawesome/react-fontawesome";
+import { star } from FontAwesomeIcon
 
 const Card = (props) => {
   return (
@@ -7,11 +9,12 @@ const Card = (props) => {
       <div className="card">
         <img src={props.product.thumbnail} className="thumnail1 card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Price - ${props.product.price}</h5>
-          <p className="card-title">Title : {props.product.title}</p>
-          <p className="card-title">category : {props.product.category}</p>
-          <h6>{props.product.description}</h6>
-          <h6>Posted Ago</h6>
+          <h5 className="price">Price - ${props.product.price}</h5>
+          <p className="title">Title : {props.product.title}</p>
+          <p className="catagery">category : {props.product.category}</p>
+          <h6 className="description">{props.product.description}</h6>
+          <p>{props.product.rating}</p>
+          <FontAwesomeIcon icon="fa-solid fa-star" />
         </div>
         <div>
           
