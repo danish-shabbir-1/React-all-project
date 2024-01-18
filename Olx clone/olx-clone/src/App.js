6da2e4b84432575ffc7439e46cb27ab7b49e1e3d
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-17 21:46:04 -0700

    working

--- Olx clone/olx-clone/src/App.js
+++ Olx clone/olx-clone/src/App.js
@@ -2,11 +2,21 @@ import AddItem from "./Components/Add Item";
 import CardContainer from "./Components/main-cards";
 import ShowCardDetail from "./Components/Show Card";
 import Router from "./Config/routing";
+import { GetAllProducts } from "../Firebase/index";
+
 
 function App() {
   return (
     <div className="App">
       <Router />
+      function fetchAllData() {
+        
+      }
+      products.map(function (item) {
+          return <div>
+
+          </div>
+      })
     </div>
   );
 }
