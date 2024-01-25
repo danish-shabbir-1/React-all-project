2ba5d274b229815d6b27a1dccbe82fc557328ded
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-25 02:27:32 -0700

    working

---
+++ Portfolio/portfolio/src/App.js
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+ <Button as="a" variant="primary">
+    Button as link
+  </Button>
+    </div>
+  );
+}
+
+export default App;
