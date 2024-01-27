76c02292558e1a2765cbc727b56bc7339569d857
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-28 02:16:08 +0500

    working

--- Firebase App/Firebase-App/src/Components/Navbar/index.jsx
+++ Firebase App/Firebase-App/src/Components/Navbar/index.jsx
@@ -36,16 +36,18 @@ function popUp() {
 
 {/* ///////////// modale here //////////// */}
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade modal-dark"  id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">DS STORE</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
-      </div>
+      <input type="text" placeholder='Title' />
+      <input type="text" placeholder='Description' />
+      <input type="number" placeholder='Price' />
+            </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
