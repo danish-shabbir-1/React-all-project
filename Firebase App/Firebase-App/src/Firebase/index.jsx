4b79f8d46a0a97f5fda8a0d67335cb1f932ac4e5
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-27 10:46:31 -0700

    working

---
+++ Firebase App/Firebase-App/src/Firebase/index.jsx
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJObtNHxmzfIczH17_AJluLCPA9vZu-Hk",
+  authDomain: "ds-store-d7a58.firebaseapp.com",
+  projectId: "ds-store-d7a58",
+  storageBucket: "ds-store-d7a58.appspot.com",
+  messagingSenderId: "590572980909",
+  appId: "1:590572980909:web:383169b9da3e246ce0d424",
+  measurementId: "G-3Y36SQ4RR4"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
