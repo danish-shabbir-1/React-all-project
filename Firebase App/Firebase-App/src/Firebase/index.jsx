a2e752ae546f427e4e94679b49a975fd2c0d804d
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-28 01:47:55 +0500

    working

--- Firebase App/Firebase-App/src/Firebase/index.jsx
+++ Firebase App/Firebase-App/src/Firebase/index.jsx
@@ -1,6 +1,5 @@
 
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, setDoc} from "firebase/firestore";
 
 
 
@@ -17,5 +16,8 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+ const app = initializeApp(firebaseConfig);
+ const auth = getAuth();
+ const db = getFirestore(app);
+
+export { auth , app}
--- Firebase App/Firebase-App/src/Views/SignUp/index.jsx
+++ Firebase App/Firebase-App/src/Views/SignUp/index.jsx
@@ -1,33 +1,72 @@
-import React from 'react'
-import { useState } from 'react'
-import './../../App.css'
-import { useNavigate } from 'react-router-dom'
-import { First, Prev } from 'react-bootstrap/esm/PageItem'
-
-
+import React from "react";
+import { useState } from "react";
+import "./../../App.css";
+import { useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../Firebase";
 const SignUp = () => {
+  const [Signup, setSignup] = useState({
+    FirstName: "",
+    LastName: "",
+    email: "",
+    passward: "",
+  });
+  const [errMas, setErrMas] = useState("");
 
-const [Signup, setSignup] = useState({
-  FirstName: '',
-  LastName: '',
-  email: '',
-  passward: '',
-})
+  // const [FirstName , LastName , email , passward] = Signup
+  const navigate = useNavigate();
 
-
-  const navigate = useNavigate()
+  const subMissionHandle = () => {
+    if (
+      !Signup.FirstName ||
+      !Signup.LastName ||
+      !Signup.email ||
+      !Signup.passward
+    ) {
+      setErrMas("Please FILL all fields");
+      return;
+    }
+    setErrMas("");
+    console.log(Signup);
+    createUserWithEmailAndPassword(auth, Signup.email, Signup.passward)
+  };
 
   return (
-    <div className='Signup-container'>
+    <div className="Signup-container">
       <h1>SignUp</h1>
-      <input onChange={(e) => setSignup((prev) => ({...prev , FirstName : e.target.value}))} type="text" placeholder='First Name'/>
-      <input onChange={(e) => setSignup((prev) => ({...prev , LastNameName : e.target.value}))} type="text" placeholder='Last Name'/>
-      <input onChange={(e) => setSignup((prev) => ({...prev , email : e.target.value}))} type="email" placeholder='Email'/>
-      <input onChange={(e) => setSignup((prev) => ({...prev , passward : e.target.value}))} type="password" placeholder='Passward'/>
-      <button>SignUp</button>
-      <p onClick={() => navigate('/login')}>Login</p>
+      <input
+        onChange={(e) =>
+          setSignup((prev) => ({ ...prev, FirstName: e.target.value }))
+        }
+        type="text"
+        placeholder="First Name"
+      />
+      <input
+        onChange={(e) =>
+          setSignup((prev) => ({ ...prev, LastName: e.target.value }))
+        }
+        type="text"
+        placeholder="Last Name"
+      />
+      <input
+        onChange={(e) =>
+          setSignup((prev) => ({ ...prev, email: e.target.value }))
+        }
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        onChange={(e) =>
+          setSignup((prev) => ({ ...prev, passward: e.target.value }))
+        }
+        type="password"
+        placeholder="Passward"
+      />
+      <b className="err">{errMas}</b>
+      <button onClick={subMissionHandle}>SignUp</button>
+      <p onClick={() => navigate("/login")}>Login</p>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
