f432c806e00801e37b200801614f9153fc67aa41
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-28 00:37:07 +0500

    working

--- Firebase App/Firebase-App/src/Firebase/index.jsx
+++ Firebase App/Firebase-App/src/Firebase/index.jsx
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, doc, setDoc} from "firebase/firestore";
 
 
 
--- Firebase App/Firebase-App/src/Views/SignUp/index.jsx
+++ Firebase App/Firebase-App/src/Views/SignUp/index.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
 import './../../App.css'
 import { useNavigate } from 'react-router-dom'
+import { First, Prev } from 'react-bootstrap/esm/PageItem'
 
 
 const SignUp = () => {
+
+const [Signup, setSignup] = useState({
+  FirstName: '',
+  LastName: '',
+  email: '',
+  passward: '',
+})
+
 
   const navigate = useNavigate()
 
   return (
     <div className='Signup-container'>
       <h1>SignUp</h1>
-      <input type="text" placeholder='First Name'/>
-      <input type="text" placeholder='Last Name'/>
-      <input type="email" placeholder='Email'/>
-      <input type="password" placeholder='Passward'/>
+      <input onChange={(e) => setSignup((prev) => ({...prev , FirstName : e.target.value}))} type="text" placeholder='First Name'/>
+      <input onChange={(e) => setSignup((prev) => ({...prev , LastNameName : e.target.value}))} type="text" placeholder='Last Name'/>
+      <input onChange={(e) => setSignup((prev) => ({...prev , email : e.target.value}))} type="email" placeholder='Email'/>
+      <input onChange={(e) => setSignup((prev) => ({...prev , passward : e.target.value}))} type="password" placeholder='Passward'/>
       <button>SignUp</button>
       <p onClick={() => navigate('/login')}>Login</p>
     </div>
