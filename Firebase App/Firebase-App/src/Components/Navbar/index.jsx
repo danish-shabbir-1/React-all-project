9d721464c7e9801941314280354b68f1b177d6dc
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-30 12:01:58 -0700

    working

--- Firebase App/Firebase-App/src/Components/Navbar/index.jsx
+++ Firebase App/Firebase-App/src/Components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./../../App.css";
-
+import image from './../../../'
 const Navbar = () => {
   function popUp() {
     const myModal = document.getElementById("myModal");
