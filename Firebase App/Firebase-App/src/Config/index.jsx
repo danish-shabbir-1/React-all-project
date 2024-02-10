87999d6e5cd3d4261c7dcb5ba7413c89029c632a
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-10 11:57:51 -0700

    working......

--- Firebase App/Firebase-App/src/Config/index.jsx
+++ Firebase App/Firebase-App/src/Config/index.jsx
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
   {
     path: "/item/:id",
     element: <div>
+      <Navbar />
       <SelectedItem />
     </div>,
   },
--- Firebase App/Firebase-App/src/Views/SelectedItem/index.jsx
+++ Firebase App/Firebase-App/src/Views/SelectedItem/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import './../../App.css'
 import { GetSingleProduct } from "../../Firebase";
 
 // 1. get product id
@@ -7,28 +8,99 @@ import { GetSingleProduct } from "../../Firebase";
 // 3 . display product
 
 function SelectedItem() {
-  const param = useParams();
-  const [product, setProduct] = useState();
-  const { id } = param;
+    const param = useParams();
+    const [product, setProduct] = useState();
+    const { id } = param;
 
-  useEffect(() => {
-    getData();
-  }, []);
+    useEffect(() => {
+        getData();
+    }, []);
 
-  async function getData() {
-    const res = await GetSingleProduct(id);
-    setProduct(res);
-  }
+    async function getData() {
+        const res = await GetSingleProduct(id);
+        setProduct(res);
+    }
 
-  console.log('product-->' , product);
+    console.log("product-->", product);
 
-  return (
-    <div>
-        <img src={product?.url} alt="" />
-      <h1>{product?.Price}</h1>
-      <h1>{product?.Description}</h1>
-    </div>
-  );
+    return (
+        <div className="details-container">
+            {/* /////////////// image slider ///////////////// */}
+            <div id="carouselExampleIndicators" className="crausel-sig-item carousel slide">
+                <div className="carousel-indicators">
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide-to="0"
+                        className="active"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                    ></button>
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide-to="1"
+                        aria-label="Slide 2"
+                    ></button>
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide-to="2"
+                        aria-label="Slide 3"
+                    ></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={product?.url} className="d-block w-100" alt="..." />
+                    </div>
+                </div>
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="prev"
+                >
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="next"
+                >
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+            {/* /////////////// image slider ///////////////// */}
+            <div className="user-detail-card card">
+                <div className="card-body">
+                    <div className="topside">
+                        <div className="leftside">
+                            <div className="image"></div>
+                        </div>
+                        <div className="rigthside">
+                            <h6>Uzair Khan</h6>
+                            <p>Member since Dec 2020</p>
+                            <span>See profile</span>
+                        </div>
+                    </div>
+                    <a href="#" className="btn btn-primary">Show Phone Number</a>
+                    <a href="#" className="btn btn-outline-primary">Chat</a>
+                </div>
+            </div>
+            <div className="user-detail-card card">
+                <div className="card-body">
+                        <div className="rigthside">
+                            <h6>Uzair Khan</h6>
+                            <p>Member since Dec 2020</p>
+                            <span>See profile</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
 }
 
 export default SelectedItem;
