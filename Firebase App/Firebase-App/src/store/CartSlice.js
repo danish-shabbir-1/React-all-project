61220098f9173259f66d4ae482e15bd3fc947f77
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-11 04:06:51 -0700

    working.......

---
+++ Firebase App/Firebase-App/src/store/CartSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const counterSlice = createSlice({
+    name: 'cart',
+    initialState: {
+      cart : []
+    },
+    reducers: {
+      UpDateCart: state => {
+        state.cart += 1
+      },
+      removeCart: state => {
+        
+      }
+    }
+  })
+  
+  export const { incremented, decremented } = counterSlice.actions
