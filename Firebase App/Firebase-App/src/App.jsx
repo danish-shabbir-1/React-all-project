773788a68d419c8cb53e10cfdb045477c46f93ef
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-13 03:37:45 -0700

    working....

--- Firebase App/Firebase-App/src/App.jsx
+++ Firebase App/Firebase-App/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { Provider } from "react-redux";
 import Router from "./Config/index";
+import DraggableMarker from "./Components/Map";
 import store from "./store";
 
 function App() {
   return (
     <Provider store={store}>
       <>
-        <Router />
+        {/* <Router /> */}
+        <DraggableMarker />
       </>
     </Provider>
   );
