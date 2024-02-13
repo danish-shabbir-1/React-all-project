87d96efa45dca82ee1b356ab0f2acc9a6e9166bb
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-13 10:46:42 -0700

    working.....

--- Firebase App/Firebase-App/src/App.jsx
+++ Firebase App/Firebase-App/src/App.jsx
@@ -1,15 +1,13 @@
 import "./App.css";
 import { Provider } from "react-redux";
 import Router from "./Config/index";
-import MapIntegrate from "./Components/Map";
 import store from "./store";
 
 function App() {
   return (
     <Provider store={store}>
       <>
-        {/* <Router /> */}
-        <MapIntegrate />
+        <Router />
       </>
     </Provider>
   );
