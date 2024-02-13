11b1e3acb86ffb79794105ea2f66a206795cf13b
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-13 04:09:45 -0700

    working.....

---
+++ Firebase App/Firebase-App/src/Components/Dragable marker/index.jsx
@@ -0,0 +1,48 @@
+import { Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'
+import React, { useState, useMemo, useCallback, useRef } from 'react';
+
+
+const center = {
+    lat: 24.859221858904903,
+    lng: 67.15474086822134,
+  }
+
+function DraggableMarker() {
+    const [draggable, setDraggable] = useState(false)
+    const [position, setPosition] = useState(center)
+    const markerRef = useRef(null)
+    const eventHandlers = useMemo(
+      () => ({
+        dragend() {
+          const marker = markerRef.current
+          if (marker != null) {
+            setPosition(marker.getLatLng())
+          }
+        },
+      }),
+      [],
+    )
+    const toggleDraggable = useCallback(() => {
+      setDraggable((d) => !d)
+    }, [])
+  
+    return (
+      <Marker
+        draggable={draggable}
+        eventHandlers={eventHandlers}
+        position={position}
+        ref={markerRef}>
+        <Popup minWidth={90}>
+          <span onClick={toggleDraggable}>
+            {draggable
+              ? 'Marker is draggable'
+              : 'Click here to make marker draggable'}
+          </span>
+        </Popup>
+      </Marker>
+    )
+  }
+  
+
+export default DraggableMarker
