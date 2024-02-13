aea67e5c7a56ea1ecb8598705294168a27f765d2
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-13 10:46:59 -0700

    working.....

--- Firebase App/Firebase-App/src/Components/Map/index.jsx
+++ Firebase App/Firebase-App/src/Components/Map/index.jsx
@@ -1,6 +1,5 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
-import DraggableMarker from '../Dragable marker';
 import React from 'react';
 
 
@@ -13,8 +12,12 @@ const MapIntegrate = () => {
     attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
     url="https://api.maptiler.com/maps/topo-v2/256/{z}/{x}/{y}.png?key=oFcV5rC5g3g8R4FqvoIG"
   />
-</MapContainer>,
-<DraggableMarker />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
   )
 }
 
