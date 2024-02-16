fb975f00bbfa86513ce82e83ca84d0209cc954c2
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-16 05:39:31 -0700

    working....

--- Firebase App/Firebase-App/src/Components/Map/index.jsx
+++ Firebase App/Firebase-App/src/Components/Map/index.jsx
@@ -5,19 +5,20 @@ import React from 'react';
 
 
 const MapIntegrate = () => {
-    const center = [24.859221858904903, 67.15474086822134]
+  const center = [24.859221858904903, 67.15474086822134]
+
   return (
-<MapContainer center={center} zoom={10}  style={{width : '100vw', height : '100vh'}}>
-  <TileLayer
-    attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-    url="https://api.maptiler.com/maps/topo-v2/256/{z}/{x}/{y}.png?key=oFcV5rC5g3g8R4FqvoIG"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
+    <MapContainer center={center} zoom={10} style={{ width: '100vw', height: '100vh' }}>
+      <TileLayer
+        attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+        url="https://api.maptiler.com/maps/topo-v2/256/{z}/{x}/{y}.png?key=oFcV5rC5g3g8R4FqvoIG"
+      />
+      <Marker draggable={true} position={center}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   )
 }
 
