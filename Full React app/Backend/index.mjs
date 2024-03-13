3bf192cdaf79a1933502eb274443974f062e5910
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-03-13 06:37:06 -0600

    conected to db setup backend

--- Full React app/Backend/index.mjs
+++ Full React app/Backend/index.mjs
@@ -1,1 +1,16 @@
 import express from "express";
+import db from './config/db.mjs'
+import routes from './routes/index.mjs'
+
+
+const app = express()
+
+db.connection.once('open', () => console.log("connected to db")).on("error", (err) => console.log("error connecting db -->", err))
+
+app.listen(3001 , function () {
+    console.log('Server is running at port 3001');
+})
+
+app.use(express.json())
+
+app.use('/' , routes)
