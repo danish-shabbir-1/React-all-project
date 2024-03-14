d50bf7cfacfcbaee6b3139fa653fa28659dfafc4
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-03-14 07:53:15 -0600

    working on crud opration

--- Full React app/Backend/routes/index.mjs
+++ Full React app/Backend/routes/index.mjs
@@ -1,9 +1,11 @@
 import express, { Router }  from "express";
 import user from './user.mjs'
+import adds from './adds.mjs'
 
 
 const router = express.Router()
 
 router.use('/users' , user)
+router.use('/ads' , adds)
 
 export default router
