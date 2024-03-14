edc6118ff747bf270e16947ecea5805b96fab82e
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-03-14 07:53:21 -0600

    working on crud opration

---
+++ Full React app/Backend/routes/adds.mjs
@@ -0,0 +1,35 @@
+import express from "express";
+import Ads from "../model/Adds.mjs";
+
+const router = express.Router()
+
+router.get('/' , async (req, res) => {
+    try {
+        const getData = await Ads.find()
+        res.send({ massage : 'Product fetch succesfully', getData})
+    } catch (e) {
+        res.send({ massage : e.massage})
+    }
+
+})
+
+router.post('/post', async (req, res) => {
+    try {
+       const postData = await Ads.create(req.body)
+        res.send({ massage : 'Product fetch succesfully'})
+    } catch (e) {
+        res.send({ massage : e.massage})   
+    }
+})
+
+router.delete('/:id' , async (req, res) => {
+    const delDataFromDb = await Ads.findByIdAndDelete(req.params.id) 
+    res.send({ massage : 'Product deleted succesfully'})
+})
+
+router.put('/:id' , async (req, res) => {
+    const updateDataFromDb = await Ads.findByIdAndUpdate(req.params.id , req.body)
+    res.send({ massage : 'Product updated succesfully'})
+})
+
+export default router
