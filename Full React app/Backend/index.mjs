465b6a1dda43272bbe04e7ee5f3d663e3900b2c5
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-03-15 04:38:27 -0600

    working on backend

--- Full React app/Backend/index.mjs
+++ Full React app/Backend/index.mjs
@@ -1,11 +1,17 @@
 import express from "express";
 import db from './config/db.mjs'
 import routes from './routes/index.mjs'
+import cors from 'cors'
 
 
 const app = express()
 
 db.connection.once('open', () => console.log("connected to db")).on("error", (err) => console.log("error connecting db -->", err))
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+  }));
+
 
 app.listen(3001 , function () {
     console.log('Server is running at port 3001');
