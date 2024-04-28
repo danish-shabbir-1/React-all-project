b5baa9d41c124c4995bb8415b4c987839410e6dc
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-04-28 02:54:21 -0700

    create folder structure Mern App

---
+++ Mern App/Mern-App/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './Components/login'
+import Signup from './Components/signup'
+
+const App = () => {
+  return (
+    <div>
+      <Login />
+      <Signup />
+    </div>
+  )
+}
+
+export default App
