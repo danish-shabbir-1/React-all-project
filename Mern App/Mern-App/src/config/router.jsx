727d9ba5039d8267aad681c2dbef9ddbf081a1de
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-04-28 06:41:23 -0700

    creating structure Mern App

---
+++ Mern App/Mern-App/src/config/router.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "../Views/login";
+import Signup from "../Views/signup";
+import Navbar from "../Components/Navbar";
+import Dashboard from "../Views/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Navbar />,<Dashboard />
+      </div>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <div>
+        <Login />
+      </div>
+    ),
+  },
+  {
+    path: "/signup",
+    element: (
+      <div>
+        <Signup />
+      </div>
+    ),
+  },
+]);
+
+function Router() {
+  return <RouterProvider router={router} />;
+}
+
+export default Router;
